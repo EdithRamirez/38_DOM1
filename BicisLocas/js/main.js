db4d7b27c6316fd08ea73c2400ef76e2a897f643
EdithRamirez <ediths.ramirez07@gmail.com>
2016-10-17 16:56:49 -0500

    ejercicio de validar los diferentes inputs

--- BicisLocas/js/main.js
+++ BicisLocas/js/main.js
@@ -1,13 +1,45 @@
 function validateForm(){
 	/* Escribe tú código aquí */
 	var nombre = document.getElementById("name").value;
-	if(nombre == null || nombre == ""){
-		alert("Ingresa tu nombre");
+	if (nombre == null ||  nombre == "") {
+		alert("Tiene que escribir su nombre");
 		return false;
 	}
-	// 	var apellido = document.getElementById("lastname").value;
-	// if(apellido == null || apellido == ""){
-	// 	alert("Ingresa tu nombre");
-	// 	return false;
-	// }
+	if(!/^[a-zA-Z]*$/g.test(document.getElementById("name").value)){
+		alert("Caracter inválido");
+		return false;
+	}
+
+	var apellido = document.getElementById("lastname").value;
+	if (apellido == null || apellido == "") {
+		alert("Tiene que escribir su apellido");
+		return false;
+	}
+	var correo = document.getElementById("input-email").value;
+	if (correo == null || correo == "") {
+		alert("Tiene que escribir su correo");
+		return false;
+	}
+	else if (!(/\S+@\S+\.\S+/.test(correo))){
+		alert("Escribe un correo válido");
+		return false;
+	}
+
+	var contrasena = document.getElementById("input-password").value;
+	if (contrasena == null || contrasena == "") {
+		alert("Tiene que escribir su contraseña");
+		return false;
+	} else if (contrasena.length < 6) {
+		alert("La contraseña debe tener al menos 6 caracteres");
+		return false;
+	} else if (contrasena == "password" || contrasena == "123456" || contrasena == "098754"){
+		alert("No se puede utilizar esa contraseña");
+		return false;
+	}
+
+	var tipoBici = document.getElementById("lista").selectedIndex;
+	if( tipoBici == null || tipoBici == 0 ) {
+		alert("Debe seleccionar una opción de la lista");
+		return false;
+	}
 }
